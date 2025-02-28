783eed2a3e75365e6512750602d4ca48858709b1
kayscotts <kayima542@gmail.com>
2025-02-28 22:36:48 +0300

    new things

--- frontend/src/store/apiSlice.js
+++ frontend/src/store/apiSlice.js
@@ -1,7 +1,7 @@
 import {createApi,fetchBaseQuery} from
  "@reduxjs/toolkit/query/react";
 
-const BASE_URL="http://localhost:8080/"
+const BASE_URL="https://kayproback.onrender.com"
 
 export const apiSlice=createApi({
   reducerPath:"fetch",
